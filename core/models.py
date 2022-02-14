2284fcd5e4657c96f03d640da92cbbb3812d272a
Mwita Chacha <jonahhchacha@gmail.com>
2022-02-14 03:36:52 +0300

    Adding copy ID function

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    spotify = models.CharField(max_length=2000,null=True)
+    youtube = models.CharField(max_length=2000,null=True)
+    tiktok = models.CharField(max_length=2000,null=True)
+
+    def __str__(self):
+        return self.name 
--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import requests
 from decouple import config
+from django.views.generic import TemplateView
 # Create your views here.
 
 def index(request):
     context=None
+    local=None
     if 'artist' in request.GET:
         artist = request.GET.get('artist')
         url = "https://spotify23.p.rapidapi.com/search/"
@@ -21,27 +23,41 @@ def index(request):
         item = artist['items']
         data = item[0]['data']
         uri=data['uri']
+        uri= uri[15:]
         profile=data['profile']
-        name=profile['name']
+        title=profile['name']
         visuals=data['visuals']
         avatar=visuals['avatarImage']
         sources=avatar['sources']
         url=sources[0]['url']
         
-        context = {'url':url,'name':name,'uri':uri}
-        
+        context = {'url':url,'name':title,'uri':uri}
+    
     if 'stats' in request.GET:
         stats = request.GET.get('stats')
 
         url = "https://songstats.p.rapidapi.com/artists/stats"
         
-        querystring = {"source":"all","spotify_artist_id":stats}
+        querystring = {"source": "all" ,"spotify_artist_id":stats}
         
         headers = {
             'x-rapidapi-host': "songstats.p.rapidapi.com",
-            'x-rapidapi-key': config("statsapi")
+            # 'x-rapidapi-key': config("statsapi")
             }
         
         responses = requests.request("GET", url, headers=headers, params=querystring).json()
-        print(responses)
+        info=responses['artist_info']
+        pic=info['avatar']
+        name=info['name']
+        
+        stats=responses['stats']
+        source=stats[0]['source']
+        data=stats[0]['data']
+        total_streams=data['streams_total']
+        # spotify=source[0]['spotify']
+        # youtube=stats[4]['youtube']
+        context = {'name':name,'data':data,'total_streams':total_streams}
+        print(name)
     return render(request, 'index.html',context)
+
+
